eabc93ca0f269e0d52cd19f395843546a41c1913
danichol <daneelnichol@outlook.com>
2021-04-10 15:38:28 -0500

    Testing

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Some$ec5etK*y'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or #postresthingy
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://lab5:postgreslab@localhost/lab5'
     SQLALCHEMY_TRACK_MODIFICATIONS = False #This is just here to suppress a warning from SQLAlchemy as it will soon be removed
 
     class DevelopmentConfig(Config):
